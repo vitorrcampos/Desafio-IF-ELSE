b940494bb7fb921dd403982dc74c3c71ac206b89
joao vitor <jaun.campos04@gmail.com>
2022-01-17 10:43:19 -0300

    desafio vnw

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+let idade = 17
+
+if (idade >= 18){
+    console.log("Parabéns, você tem 18 anos ou mais!")
+} else{
+    console.log("Você ainda não tem 18 anos!")
+}
+
+let humano = true
+
+if (idade >= 18 && humano == true) {
+    console.log("A condição deu true")
+} else{
+   console.log("A condição deu false")
+}
+
+
+let mes = "Maio"
+
+if (mes === "Dezembro" || mes === "Janeiro") { console.log("seu aniversário é em dezembro ou janeiro") }
+else{
+    console.log(`seu aniversário é em ${mes}` )
+}
+
+
+
